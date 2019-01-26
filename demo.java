aa71b0aabe2eacd5d7946bec693cd01228c437b5
zhangmuye <2315293476@qq.com>
2019-01-26 18:59:12 +0800

    New Java File - demo.java Create

---
+++ demo.java
@@ -0,0 +1,9 @@
+﻿public class Demo {
+	
+	public static void main(String args[]){
+		System.out.println("世界2，你好！Demo");
+		System.out.println("世界2，你好！Demo");
+		System.out.println("世界2，你好！Demo");
+		System.out.println("世界2，你好！Demo");
+	}
+}

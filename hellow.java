41a917f9a893e25a903a11e9464b045ee6cc989c
zhangmuye <2315293476@qq.com>
2019-01-26 18:52:46 +0800

    Change Two Files

--- hellow.java
+++ hellow.java
@@ -2,5 +2,8 @@ public class Hello {
 	
 	public static void main(String args[]){
 		System.out.println("世界2，你好！");
+		System.out.println("世界2，你好！");
+		System.out.println("世界2，你好！");
+		System.out.println("世界2，你好！");
 	}
 }

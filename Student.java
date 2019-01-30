8d5c008fe817ff2c35cf9ff394e7d5a823532e7e
zhangmuye <2315293476@qq.com>
2019-01-30 15:36:26 +0800

    New Java File - Student.java Create

---
+++ Student.java
@@ -0,0 +1,19 @@
+package com.zmy.student.impl;
+
+/**
+ * @author zmy
+ * @date 2019.01.29
+ * @version 1.0
+ */
+
+public interface Student {
+
+   public final String STUDENT_NAME = "陈伟同学";
+
+    /**
+     *  获得学生的姓名
+     * @return 学生姓名
+     */
+    String getAuthorName();
+
+}
